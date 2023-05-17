89e73150a2a44423cd511b6409b23d89559a49bf
PuggMonkey <olirhys@icloud.com>
2023-05-17 22:23:38 +0100

    Initial commit

---
+++ v14 Discord Package/src/Events/ready.js
@@ -0,0 +1,28 @@
+module.exports = {
+    name: 'ready',
+    once: true,
+    async execute(client) {
+        console.log('✅🟢 The Bot Is Ready And Online! 🟢✅');
+
+        async function pickPresence () {
+            const option = Math.floor(Math.random() * statusArray.length);
+
+            try {
+                await client.user.setPresence({
+                    activities: [
+                        {
+                            name: statusArray[option].content,
+                            type: statusArray[option].type,
+
+                        },
+                    
+                    ],
+
+                    status: statusArray[option].status
+                })
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    },
+};
